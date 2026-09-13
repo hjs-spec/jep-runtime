ec5ac5f547af567cb01216d1c4fc8b24cb6b2cb1
Yuqiang Wang <10815391+schchit@users.noreply.github.com>
2026-09-13 11:06:47 +0800

    Prepare verified distributions and complete deployment support

--- jep_runtime/core/version.py
+++ jep_runtime/core/version.py
@@ -3,7 +3,7 @@
 JEP_DRAFT_VERSION = "jep-v06"
 JEP_DRAFT_REFERENCE = "Judgment Event Protocol Internet-Draft v06"
 JEP_DRAFT_REPOSITORY = "jep-v06"
-RUNTIME_VERSION = "0.1.0"
+RUNTIME_VERSION = "0.1.1"
 
 __all__ = [
     "JEP_DRAFT_VERSION",
